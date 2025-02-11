b950a3d034ee04f676d0fd667d2efb9e868b3fe6
Tomasz Okoń <tomekokon75@gmail.com>
2025-02-11 22:56:37 +0100

    x

--- Engine/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tomaszokon/Documents/Chess/Engine
-# Build directory: /Users/tomaszokon/Documents/Chess/Engine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/googletest-build")
